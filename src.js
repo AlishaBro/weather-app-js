7ac5b3b522d7e9bcf89f3abb1ad46691ce7ab913
Alisha Brodin <80694305+AlishaBro@users.noreply.github.com>
2023-10-25 12:40:57 +0200

    Added the weather Forecast
    Added the JS logic to insert html forecast elements from JS.
    
    deleted the dummy html forecast code

--- src.js
+++ src.js
@@ -30,8 +30,32 @@ function processApiResponse(response){
   let htmlWeatherIcon=document.querySelector("#main-icon");
   htmlWeatherIcon.src=response.data.condition.icon_url;
   updateDate(response.data.time);
-  
+  debugger;
+  displayForecast();
 };
+
+function displayForecast(){
+let forecastElement=document.querySelector(".forecastWeather");
+console.log(forecastElement);
+
+let days=["Sun","Mon","Tue","Wed","Thu","Fri","Sat"];
+forecastElement.innerHTML=`<div class="row forecast mt-4 forecastWeather">`;
+days.forEach((day,idx )=> {
+  if (idx<6){
+forecastElement.innerHTML=forecastElement.innerHTML +` <div class="col-2">
+                            <div class="day">${day}</div>
+                            <div class="icon">
+                                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAABHNCSVQICAgIfAhkiAAAAAlwSFlzAAALEgAACxIB0t1+/AAAAdVJREFUaN7tmc1thDAQRimBElwCJVBCSvAxR5fgEiiBEiiBErhyIx24A2cc2WhiAf4ZA1rJkZ4UZZPN9/AwHrON1rr5ZJoqUAWqQBWoAlWgxJf++WaAAGZAAdpD2dfM7zDS/yopAGE6YDoIHMLIdK8KQIAWGIAtQ8Bh/r59bQWQjCBILCkSJIF1XVuAA9Jivm9ROd0ukS0AQTtgA7SH+Vn31EoEBSAMA2YUUAHiJDyWcCtBuidIArZEroJewVEpjQSJjiIgMsMbpHdjf53sCcEWSxEYCQKOyZQhkshZBZYkYEtHeLVPQSGJnHIS0QI2/FIo+L+VILTXOUVA3BD+D3Q/pAqoFIEebUxFQQLJN/Ojo0TEqDG/JgBv1hdgeVNAP4CKPSvkCKiCQc1KSMRs2+x902hO/Z4cYFhgWOQHY8zo9hOKgCCGH71BEXcqHjEBKDft5gowypVH4YeLgKE9ZSO10cxz7z7TFJqxOEUgZxyYbPi+0M4uSRuZPYCnCPBA6TwrYCWWyFbJImo/FTMpM6pAG5CYvDO0LDii7x2JNAtdSGxuQyp41Q87UqkHW8NJzYsbw+8d6Y5Hi+7qbw8IyOIPd9HRVD8qUD8fqAJVoApUgSrwqfwCJ6xaZshM+xMAAAAASUVORK5CYII=" alt="weather-icon">
+                            </div>
+                            <div class="maxtemp minTemp">
+                               4°<span> 9°</span> 
+                            </div>
+                        `
+
+}})
+forecastElement.innerHTML=forecastElement.innerHTML +` </div`
+};
+
 
 
 UpdateDefaultCity("Berlin") ;
@@ -131,7 +155,3 @@ function handleClick(event)
 
 let currentTempButton=document.querySelector(".current-temp-btn");
 currentTempButton.addEventListener("click",handleClick);
-
-
-
-
